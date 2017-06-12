7c87b44ae6b5c96e6b95975273421f59e6f2532c
UG Lemondo <uglemondo@UGs-iMac-3.local>
2017-06-12 17:36:14 +0400

    samalamadumalama

---
+++ Instagram/TableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  TableViewCell.swift
+//  Instagram
+//
+//  Created by UG Lemondo on 6/12/17.
+//  Copyright © 2017 UG Lemondo. All rights reserved.
+//
+
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var avatar: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var picture: UIImageView!
+    @IBOutlet weak var views: UILabel!
+    @IBOutlet weak var uploadDate: UILabel!
+    
+}
---
+++ Instagram/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  Instagram
+//
+//  Created by UG Lemondo on 6/12/17.
+//  Copyright © 2017 UG Lemondo. All rights reserved.
+//
+
+
+import UIKit
+
+class User {
+    var name: String!
+    var avatar: UIImage!
+    var picture: UIImage!
+    var views: String!
+    var upload: String!
+}
